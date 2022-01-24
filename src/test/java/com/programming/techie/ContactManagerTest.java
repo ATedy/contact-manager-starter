05ff9a5331f548893ec4a4579f0e52bb640f492a
Amanuel Tedros <73557032+ATedy@users.noreply.github.com>
2022-01-24 12:23:47 +0000

    Null test scenarios created for the fields and passed the tests

--- src/test/java/com/programming/techie/ContactManagerTest.java
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -1,6 +1,7 @@
 package com.programming.techie;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ContactManagerTest {
@@ -19,4 +20,36 @@ class ContactManagerTest {
               .isPresent());
 
    }
+   // Neagtive test Scenarios
+
+   // When first name is null
+   @Test
+   @DisplayName("Should not Create Contact When First Name is Null")
+   public void shouldThrowRuntimeExceptionWhenFirstNameIsNull() {
+      ContactManager contactManager = new ContactManager();
+      Assertions.assertThrows(RuntimeException.class, (() -> {
+         contactManager.addContact("Jon", null, "0123456789");
+      }));
+   }
+
+   // When last name is null
+   @Test
+   @DisplayName("Should not Create Contact When Last Name is Null")
+   public void shouldThrowRuntimeExceptionWhenLastNameIsNull() {
+      ContactManager contactManager = new ContactManager();
+      Assertions.assertThrows(RuntimeException.class, (() -> {
+         contactManager.addContact(null, "Doe", "0123456789");
+      }));
+   }
+
+   // When phone number is null
+   @Test
+   @DisplayName("Should not Create Contact When Phone Number is Null")
+   public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull() {
+      ContactManager contactManager = new ContactManager();
+      Assertions.assertThrows(RuntimeException.class, (() -> {
+         contactManager.addContact("Jon", "Doe", null);
+      }));
+   }
+
 }
